87ab84d87d2562d42fda86491712736304503a61
Fernando <fernando.imkt@hotmail.com>
2023-12-05 09:39:04 -0500

    refact: [REF-04] [Fernando] new data readme-2, App.js

--- README-2.md
+++ README-2.md
@@ -1,6 +1,21 @@
 # me
 
-For new project:
+For new project in GitHub:
 git add, git commit, git push.
 
+
+Requeriments:
+Node.js, nvm (https://github.com/nvm-sh/nvm) --> (Excecute: curl -o- httpS://raw.git...........) -->(it from my branch).
+
+
+Proccess:
+npx create-react-app my-app, cd my-app, npm start --> (localhost:3000)
+
+
+In .gitignore:
+add --> package-lock.json
+
+
+In termux:
+Nodejs, npx.
 
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+
+	const saludo = "Hola mundo!    ... ( editado por Fernando Gavilanes ) ...";
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +13,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+	  <p>
+	  {saludo}
+	  </p>
         <a
           className="App-link"
           href="https://reactjs.org"
